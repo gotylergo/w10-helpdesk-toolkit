50261998dad49f93a8bac7fc3b0797555d7e577a
Tyler Kocheff <tk03759@gmail.com>
2019-08-02 19:36:32 -0700

    Add bitlocker,

--- provision-pc.ps1
+++ provision-pc.ps1
@@ -108,6 +108,27 @@ if (-not (Test-Path 'HKLM:\SOFTWARE\pcProv')) {
         Write-Error "Couldn't install Chocolatey. Try manually." >> $LogFile
     }
 
+    # Encrypt System Drive
+    Write-Output "Encrypting system drive..." >> $LogFile
+    Start-Process 'manage-bde.exe' -ArgumentList " -protectors -add $env:SystemDrive -recoverypassword" -Verb runas -Wait
+    Start-Process 'manage-bde.exe' -ArgumentList " -on -usedspaceonly $env:SystemDrive -em aes256 " -Verb runas -Wait
+    #Backing Password file to the server
+    $pcSerialNumber = (Get-WmiObject win32_bios).SerialNumber
+#     New-PSDrive -Name "z" -PSProvider "Filesystem" -Root "" -Credential $credential
+    if ($?) {
+        New-Item -Path "z:\" -Name "$env:computername $pcSerialnumber" -ItemType "directory"
+        (Get-BitLockerVolume -MountPoint C).KeyProtector.recoverypassword -match '\S\' > "z:\$env:computername $pcSerialnumber\Bitlocker Recovery Key.txt"
+        if ($?) {
+            Write-Output "Bitlocker key successfully saved." >> $LogFile
+        }
+        else {
+            Write-Error "Saving Bitlocker key failed. Try manually." >> $LogFile
+        }
+    }
+    else {
+        Write-Error "Encryption failed, try manually."
+    }
+
     # Increment the registry value to resume where we left off after reboot
     Set-ItemProperty -Path HKLM:\SOFTWARE\pcProv -Name "Status" -Value 1
     if ($?) {
@@ -120,7 +141,7 @@ if (-not (Test-Path 'HKLM:\SOFTWARE\pcProv')) {
     Set-ScheduledRebootTask
 
     # Join to domain and restart
-    Add-Computer -DomainName $domainName -NewName $newPCName -Credential $credential -Restart -Force
+    Add-Computer -DomainName $domainName -Server HQ-corpdc01 -NewName $newPCName -Credential $credential -Restart -Force
 }
 # Part 2: If script has run, check reg key to determine where to continue
 else {
@@ -129,40 +150,69 @@ else {
     if ($regStatus -eq 1) {
         Write-Output "Registry key value is: 1. Continuing from Part 2.1" >> $LogFile
 
-        ## Run Updates
-        # Install-PackageProvider NuGet -Force
-        # Import-PackageProvider NuGet -Force
-        # Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-        # Install-Module PSWindowsUpdate
-        # Get-Command –module PSWindowsUpdate
-        # Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$false
-        # Get-WUInstall –MicrosoftUpdate –AcceptAll –AutoReboot
+        # still working on this
 
         # Increment the registry value to resume where we left off after reboot
         Set-ItemProperty -Path HKLM:\SOFTWARE\pcProv -Name "Status" -Value 2
         if ($?) {
             Write-Output "Registry key changed to value: 2." >> $LogFile
         }
+        Restart-Computer -Force
 
     }
     # Script Part 2.2
     elseif ($regStatus -eq 2) {
         Write-Output "Registry key value is: 2. Continuing from Part 2.2" >> $LogFile
 
-        # Still working on this part
+        # Still working on this
+        # Install Windows Updates
+        Install-PackageProvider -Name -NuGet -Force
+        Install-Module PSWindowsUpdate -Force -Confirm:$false
+        Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$false
+        do {
+            Get-WUInstall -MicrosoftUpdate -AcceptAll -AutoReboot
+        }
+        while ((Get-WUList) -ne 0)
+        if ((Get-WUList) -eq 0) {
+            Write-Output "Windows Updates installed." >> $LogFile
+        }
+        else {
+            Write-Error "Windows Updates not complete. Try manually." >> $LogFile
+        }
 
         Write-Output "Provisioning complete." >> $LogFile
+
+        # Play complete tones
+        function b($a, $b) {
+            [console]::beep($a, $b)
+        }
+        function s($a) {
+            sleep -m $a
+        }
+        b 660 100;
+        s 150;
+        b 660 100;
+        s 300;
+        b 660 100;
+        s 300;
+        b 510 100;
+        s 100;
+        b 660 100;
+        s 300;
+        b 770 100;
+        s 550;
+
         $wshell = New-Object -ComObject Wscript.Shell
         $wshell.Popup("Provisioning completed", 0, "provPC")
         Unregister-ScheduledTask -TaskName pcProv -Confirm:$false
         if ($?) {
-            Write-Output "Unregistered scheduled task."
+            Write-Output "Unregistered scheduled task." >> $LogFile
         }
         else {
-            Write-Error "Scheduled task 'pcProv' could not be deleted. Try manually."
+            Write-Error "Scheduled task 'pcProv' could not be deleted. Try manually." >> $LogFile
         }
     }
     else {
-        Throw "Reg key exists, but doesn't match any part of the script. Exiting." >> $LogFile
+        Throw "Reg key exists, but doesn't match any part of the script (or the script was already completed on this machine). Exiting." >> $LogFile
     }
 }
