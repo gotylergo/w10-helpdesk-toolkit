ee1abd211d083da8a811e144a258134afd5f5331
Tyler Kocheff <tk03759@gmail.com>
2019-10-18 16:12:35 -0700

    Implement try/catch and cleanup

--- term-processor.ps1
+++ term-processor.ps1
@@ -1,5 +1,5 @@
 # Config variables
-$DCserver = Read-Host "What Active Directory Domain Controller should we be working on?"
+$DCserver = Read-Host "What domain controller should we connect to?"
 
 # Get Input to Define Variables
 
@@ -8,64 +8,64 @@ $NameFirst = Read-Host 'User First Name'
 $NameLookup = "*$NameLast* *$NameFirst*"
 
 # Search for user in AD
-$ADUser = Get-ADuser -Filter { name -like $NameLookup } -Server ($DCserver) | Select-Object name, samaccountname | Sort-Object samaccountname
-if ($?) {
-    Write-Host "Found user: $ADUser"
-    Pause
+Try {
+  $ADUser = Get-ADuser -Filter { name -like $NameLookup } -Server ($DCserver) | Select-Object name, samaccountname | Sort-Object samaccountname
+  Write-Host "Found user: $ADUser"
+  Pause
+
 }
-else {
-    EXIT
+Catch {
+  EXIT
 }
 $Username = $ADuser.samaccountname
-Write-Host "Found user: $Username"
 
 # Declare Create-Password function
 # Credit iRon: https://stackoverflow.com/questions/37256154/powershell-password-generator-how-to-always-include-number-in-string
 Function MakeUp-String([Int]$Size = 16, [Char[]]$CharSets = "ULNS", [Char[]]$Exclude) {
-    $Chars = @(); $TokenSet = @()
-    If (!$TokenSets) {
-        $Global:TokenSets = @{
-            U = [Char[]]'ABCDEFGHIJKLMNOPQRSTUVWXYZ'                                #Upper case
-            L = [Char[]]'abcdefghijklmnopqrstuvwxyz'                                #Lower case
-            N = [Char[]]'0123456789'                                                #Numerals
-            S = [Char[]]'!"#$%&''()*+,-./:;<=>?@[\]^_`{|}~'                         #Symbols
-        }
+  $Chars = @(); $TokenSet = @()
+  If (!$TokenSets) {
+    $Global:TokenSets = @{
+      U = [Char[]]'ABCDEFGHIJKLMNOPQRSTUVWXYZ'                                #Upper case
+      L = [Char[]]'abcdefghijklmnopqrstuvwxyz'                                #Lower case
+      N = [Char[]]'0123456789'                                                #Numerals
+      S = [Char[]]'!"#$%&''()*+,-./:;<=>?@[\]^_`{|}~'                         #Symbols
     }
-    $CharSets | ForEach {
-        $Tokens = $TokenSets."$_" | ForEach { If ($Exclude -cNotContains $_) { $_ } }
-        If ($Tokens) {
-            $TokensSet += $Tokens
-            If ($_ -cle [Char]"Z") { $Chars += $Tokens | Get-Random }             #Character sets defined in upper case are mandatory
-        }
+  }
+  $CharSets | ForEach {
+    $Tokens = $TokenSets."$_" | ForEach { If ($Exclude -cNotContains $_) { $_ } }
+    If ($Tokens) {
+      $TokensSet += $Tokens
+      If ($_ -cle [Char]"Z") { $Chars += $Tokens | Get-Random }             #Character sets defined in upper case are mandatory
     }
-    While ($Chars.Count -lt $Size) { $Chars += $TokensSet | Get-Random }
-    ($Chars | Sort-Object { Get-Random }) -Join ""                                #Mix the (mandatory) characters and output string
+  }
+  While ($Chars.Count -lt $Size) { $Chars += $TokensSet | Get-Random }
+  ($Chars | Sort-Object { Get-Random }) -Join ""                                #Mix the (mandatory) characters and output string
 }; Set-Alias Create-Password MakeUp-String -Description "Generate a random string (password)"
 
 # Reset password
 $termPwd = Create-Password
-Set-ADAccountPassword -Server $DCserver -Identity $Username -NewPassword (ConvertTo-SecureString -AsPlainText $termPwd -Force)
-if ($?) {
-    Write-Host "Password reset to: $termPwd"
+Try {
+  Set-ADAccountPassword -Server $DCserver -Identity $Username -NewPassword (ConvertTo-SecureString -AsPlainText $termPwd -Force)
+  Write-Host "Password reset to: $termPwd"
 }
-else {
-    Write-Host "User password could not be changed to: $termPwd"
-    Read-Host -Prompt "Change the term's password to this manually. Then press Enter to continue"
+Catch {
+  Write-Host "User password could not be changed to: $termPwd"
+  Read-Host -Prompt "Change the term's password to this manually. Then press Enter to continue"
 }
 
 # Remove AD Groups
 
 Write-Host "Backing up AD groups..."
 
-Get-ADPrincipalGroupMembership -Server $DCserver ABarcelo | select name | export-csv "ABarcelo-groups.csv"
-Write-Output "Backed up to $Username-groups.csv"
-
-if ($?) {
-    # $Groups = Import-csv "$Username-groups.csv"
-    foreach ($Group in $Groups) {
-        # Remove-ADGroupMember -Server $DCserver -Identity $Group -Members $Username
-        Write-Output "Group $Group removed."
-    }
-} else {
-    Write-Output "Couldn't backup/remove groups. Try manually."
+Try {
+  Get-ADPrincipalGroupMembership -Server $DCserver $Username | select name | export-csv "$Username-groups.csv"
+  Write-Output "Backed up to $Username-groups.csv"
+  #     $Groups = Import-csv "$Username-groups.csv"
+  #     foreach ($Group in $Groups) {
+  #         Remove-ADGroupMember -Server $DCserver -Identity $Group -Members $Username
+  #         Write-Output "Group $Group removed."
+  #     }
+}
+Catch {
+  Write-Output "Couldn't backup/remove groups. Try manually."
 }
