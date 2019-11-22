21625f85ea5e139fd7fea26c149ae4e26b1c8e7e
Tyler Kocheff <tk03759@gmail.com>
2019-11-21 22:56:43 -0800

    Generalize variables

--- remote-ps.bat
+++ remote-ps.bat
@@ -1,5 +1,6 @@
 REM Setting PC up for remote powershell and joining to domain
 powershell.exe -Command "Enable-PSRemoting -Force"
 set /p NewPCName=Enter the NewPCName.
-set DomainName=carsdirect.win
-powershell.exe -Command "Add-Computer -DomainName %DomainName% -Server HQ-corpdc01 -NewName %NewPCName% -Credential (Get-Credential) -Restart -Force"
+set /p DomainName=Enter the domain to join
+set /p Server=Enter the domain controller
+powershell.exe -Command "Add-Computer -DomainName %DomainName% -Server %Server% -NewName %NewPCName% -Credential (Get-Credential) -Restart -Force"
